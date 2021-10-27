4976b237b61998070d10171d5cb9754ab79aa433
Lee Eun-pyo <gm20813@gmail.com>
2021-10-27 15:45:15 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const foodArray = ['a', 'b', 'c', 'd', 'e'];
+
+function Food({fav, index}) {
+    console.log(fav);
+    return (
+        <h2>{index} : I like {fav}.</h2>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Hello</h1>
+
+            {foodArray.map((food, i) => <Food fav={food} index={i} />)}
+        </div>
+    );
 }
 
 export default App;
